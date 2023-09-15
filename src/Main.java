9b57e0f1bf720985b320887d7530d1071c0ed7c0
jamie <jamiek0414@gmail.com>
2023-09-15 16:01:33 -0400

    Final commit!!(fixed)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args)
     {
-        int fallCost = 150;
-        int winterCost = 200;
-        int springCost = 250;
-        int summerCost = 300;
-        int homeMaintenance = fallCost + winterCost + springCost + summerCost;
-
-        System.out.println("Your annual home maintenance cost is " + homeMaintenance + " dollars");
+        int purchasePrice = 100;
+        double salesTax = purchasePrice * .05;
+        System.out.println("Your sales tax on a purchase of " + "$" + purchasePrice + " is " + "$" + salesTax);
     }
 }
 //blah
